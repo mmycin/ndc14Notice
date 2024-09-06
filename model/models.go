237a13dd858d7c8fa0dea279806f8c99b74fa422
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2024-09-06 23:54:58 +0600

    Update models.go

--- model/models.go
+++ model/models.go
@@ -3,7 +3,7 @@ package model
 import "github.com/mmycin/mongorm/model"
 
 type Notice struct {
-	model.BaseModel
+	model.BaseModel `-`
 	Date    string `json:"date"`
 	Title	string	`json:"title"`
 	Content string `json:"content"`
